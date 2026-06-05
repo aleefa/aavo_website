c801cf4a4e94d8d8cf87034c19de1d4b5f040da3
Aleefa MP <aleefamp76@gmail.com>
2026-06-05 15:51:31 +0530

    Our Commitment fixed

--- app/privacy-policy/page.tsx
+++ app/privacy-policy/page.tsx
@@ -401,7 +401,7 @@ export default function PrivacyPolicy() {
                 </div> */}
                 <NumBadge n="04" />
                 <h3 className="text-2xl font-bold text-gray-900 leading-9 tracking-tight m-0">
-                  Our Commitment: What We Do NOT Do
+                  Our Commitment: What We Do Not Do
                 </h3>
               </div>
               <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
