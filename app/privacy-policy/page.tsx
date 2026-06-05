d1b495e8d310c26e1b92bf68a34a8f47422ed2e9
Aleefa MP <aleefamp76@gmail.com>
2026-06-05 15:45:17 +0530

    font size fixed

--- app/privacy-policy/page.tsx
+++ app/privacy-policy/page.tsx
@@ -420,7 +420,7 @@ export default function PrivacyPolicy() {
                       />
                     </svg>
                   </div>
-                  <p className="font-bold md:text-sm text-lg mb-2">
+                  <p className="font-bold md:text-base text-lg mb-2">
                     No Selling Data
                   </p>
                   <p className="text-[#444748] text-base md:text-[15px] leading-relaxed">
@@ -444,7 +444,7 @@ export default function PrivacyPolicy() {
                       />{" "}
                     </svg>
                   </div>
-                  <p className="font-bold text-sm mb-2">Private Contacts</p>
+                  <p className="font-bold text-base mb-2">Private Contacts</p>
                   <p className="text-[#444748] text-base md:text-[15px] leading-relaxed">
                     We do not share your private contact information with other
                     users except as necessary for the service.
