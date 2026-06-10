985f36b795363dd492a5733d379878a7eaa56aa0
Aleefa MP <aleefamp76@gmail.com>
2026-06-10 16:21:16 +0530

    chnaged manali image

--- lib/mockData.ts
+++ lib/mockData.ts
@@ -214,7 +214,7 @@ export const MOCK_BLOGS: Blog[] = [
       },
       {
         type: 'image',
-        imageUrl: '/manali1.png',
+        imageUrl: 'https://images.unsplash.com/photo-1592548890095-cd2a7aeca5ac?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
         caption: 'Misty winter mornings along the pine-lined ridges of Manali.'
       },
       {
