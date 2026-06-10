781f85a975c1ec638f20466b7379681e9063a8ec
Aleefa MP <aleefamp76@gmail.com>
2026-06-10 14:22:32 +0530

    NOT fixed

--- app/terms-and-conditions/page.tsx
+++ app/terms-and-conditions/page.tsx
@@ -21,7 +21,7 @@ const termsData = [
           <li>
             We facilitate initial contact but do not moderate the relationship.
           </li>
-          <li>AAVORide is Not a transportation company or a fleet owner.</li>
+          <li>AAVORide is not a transportation company or a fleet owner.</li>
         </ul>
       </>
     ),
